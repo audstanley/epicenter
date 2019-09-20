0e88f7e1facbdc2fe8282a05cd52dd5dc98264d1
Richard Stanley <audstanley@gmail.com>
2019-09-19 21:32:23 -0700

    first commit

---
+++ imports/ui/body.js
@@ -0,0 +1,12 @@
+import { Template } from 'meteor/templating';
+
+import './body.html';
+ 
+Template.body.helpers({
+  tasks: [
+    { text: 'This is task 1' },
+    { text: 'This is task 2' },
+    { text: 'This is task 3' },
+  ],
+  someShit: "poop",
+});
---
+++ server/main.js
@@ -0,0 +1,44 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import '../imports/api/tasks.js';
+
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+let globalCounter = 0;
+
+Meteor.startup(() => {
+  // code to run on server at startup
+  console.log("it's working")
+});
+
+Meteor.methods({
+  helloWorld (data) {
+    fs.writeFile('./poop.txt', `the global counter * 2 = ${(data*2).toString()}`, (err)=> {
+      if (err) {
+         console.log(err)
+      } else {
+        console.log('saved file');
+      }
+    });
+
+    let d = fetch('http://api.openweathermap.org/data/2.5/weather?zip=92630,us&APPID=06eb04611b508e74c1a6f0f4e2968ca2')
+      .then(d => d.json())
+      .then(d => {
+        let kelvinToFer =  Math.round( (((d.main.temp-273.15)*9)/5)+32 )
+        console.log(kelvinToFer);
+        return `${kelvinToFer}°F`;
+      });
+    
+    return d;
+  },
+
+  globalCounterFunc(data) {
+    let newGlobalCounter = globalCounter + 1;
+    globalCounter = newGlobalCounter;
+    return newGlobalCounter;    
+  }
+
+  
+
+})
