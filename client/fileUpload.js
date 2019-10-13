352b640b2adad268c0027ffe49a2dd6f9635c5c9
Richard Stanley <audstanley@gmail.com>
2019-10-13 16:52:10 -0700

    new eslinting, and mocha tests for unit testing

--- client/fileUpload.js
+++ client/fileUpload.js
@@ -1,8 +1,9 @@
-import { Template }    from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-import { Meteor } from 'meteor/meteor';
+import { Template } from "meteor/templating";
+import { ReactiveVar } from "meteor/reactive-var";
+import { Meteor } from "meteor/meteor";
+import { FilesCollection } from "meteor/ostrio:files";
 
-Template.fileUpload.onCreated(function () {
+Template.fileUpload.onCreated(function() {
   this.currentUpload = new ReactiveVar(false);
 });
 
@@ -13,32 +14,53 @@ Template.fileUpload.helpers({
 });
 
 Template.fileUpload.events({
-  'change #fileInput'(e, template) {
-    if (e.currentTarget.files && e.currentTarget.files[0]) {
+  "change #fileInput"(event, templateInstance) {
+    if (event.currentTarget.files && event.currentTarget.files[0]) {
       // We upload only one file, in case
       // multiple files were selected
-      const upload = Images.insert({
-        file: e.currentTarget.files[0],
-        streams: 'dynamic',
-        chunkSize: 'dynamic',
-      }, false);
+      const upload = Images.insert(
+        {
+          file: event.currentTarget.files[0],
+          streams: "dynamic",
+          chunkSize: "dynamic"
+        },
+        false
+      );
 
-      upload.on('start', function () {
-        template.currentUpload.set(this);
+      upload.on("start", function() {
+        templateInstance.currentUpload.set(this);
       });
 
-      upload.on('end', function (error, fileObj) {
+      upload.on("end", function(error, fileObj) {
         if (error) {
-          alert('Error during upload: ' + error);
+          alert("Error during upload: " + error);
+          $(`#uploadFeedback`).append(
+            `<div class="unsuccessfulUpload">
+            <p>Error uploading: <strong>${fileObj.name}</strong></p>
+            </div>`
+          );
+          $(`.unsuccessfulUpload`).css({ "background-color": "#FFD9CD" });
         } else {
           //alert('File "' + fileObj.name + '" successfully uploaded');
-          console.log(fileObj);
-          Meteor.call('makeTorrentFromUploadedFile', fileObj, (error, result) => {
-            if (error)console.log(error);
-            else console.log(result);
-          });
+          $(`#uploadFeedback`).append(
+            `<div class="successfulUpload">
+              <p>Successfully uploaded:
+              <strong>${fileObj.name}</strong></p>
+            </div>`
+          );
+
+          $(`.successfulUpload`).css({ "background-color": "#DEFFEC" });
+
+          Meteor.call(
+            "makeTorrentFromUploadedFile",
+            fileObj,
+            (error, result) => {
+              if (error) console.log(error);
+              else console.log(result);
+            }
+          );
         }
-        template.currentUpload.set(false);
+        templateInstance.currentUpload.set(false);
       });
 
       upload.start();
@@ -46,22 +68,8 @@ Template.fileUpload.events({
   }
 });
 
-
 const Images = new FilesCollection({
-  collectionName: 'Files',
+  collectionName: "Files",
   allowClientCode: false, // Disallow remove files from Client
-  storagePath: "../../../../../public/Files",
-  onBeforeUpload(file) {
-    return true;
-  }
+  storagePath: "../../../../../public/Files"
 });
-
-// if (Meteor.isClient) {
-//   Meteor.subscribe('files.images.all');
-// }
-
-// if (Meteor.isServer) {
-//   Meteor.publish('files.images.all', function () {
-//     return Images.find().cursor;
-//   });
-// }
--- server/torrentFunctions/torrentFunctions.js
+++ server/torrentFunctions/torrentFunctions.js
@@ -1,93 +1,87 @@
-const createTorrent = require('create-torrent')
-const fs = require('fs')
-const Transmission = require('transmission')
+const createTorrent = require("create-torrent");
+const fs = require("fs");
+const Transmission = require("transmission");
 const transmission = new Transmission({
-  host: 'localhost', // default 'localhost'
+  host: "localhost", // default 'localhost'
   port: 9091, // default 9091
-  username: 'audstanley', // default blank
-  password: 'password', // default blank
+  username: "audstanley", // default blank
+  password: "password", // default blank
   ssl: false, // default false use https
-  url: '/transmission/rpc', // default '/transmission/rpc'
-})
-
+  url: "/transmission/rpc" // default '/transmission/rpc'
+});
 
 // This function will take a filepath, and return an object of the b64 string of the binary file
 // as well as the filename.
 function createTorrentFromFileName(filename, storagepath) {
-    return new Promise((resolve, reject) => {
-        console.log(`${process.cwd()}/${filename}`);
-        createTorrent(`${process.cwd()}/${filename}`, (err, tor) => {
-            if (err) reject(err);
-            else {
-                // console.log(`${process.cwd()}/${filename}`);
-                // console.log(`${process.cwd()}/${storagepath}`)
-                // console.log(`${tor.toString('base64')}`)
-                // console.log(`${tor}`)
-                resolve({'name':`${process.cwd()}/${filename}`,'b64':`${tor.toString('base64')}`, 'tor': tor, 'storagePath': `${process.cwd()}/${storagepath}`  });
-            }
-        })
-    })
+  return new Promise((resolve, reject) => {
+    console.log(`${process.cwd()}/${filename}`);
+    createTorrent(`${process.cwd()}/${filename}`, (err, tor) => {
+      if (err) reject(err);
+      else {
+        resolve({
+          name: `${process.cwd()}/${filename}`,
+          b64: `${tor.toString("base64")}`,
+          tor: tor,
+          storagePath: `${process.cwd()}/${storagepath}`
+        });
+      }
+    });
+  });
 }
 
 // takes and obj.tor Buffer, and saves it to the ./uploads directory in the project folder
 function saveFileToTorrentFolder(obj) {
-    return new Promise((resolve, reject) => {
-        if (!(obj.name && obj.tor)) reject('saveFileToTransmissionError: { name: string, tor: Buffer}');
-        else {
-            // console.log(`writing file: ${obj.name}.torrent`)
-            fs.writeFile(`${obj.name}.torrent`, obj.tor, err => {
-                if(err) reject(err);
-                else resolve(obj);
-            })
-
-                
-        }
-    })
+  return new Promise((resolve, reject) => {
+    if (!(obj.name && obj.tor))
+      reject("saveFileToTransmissionError: { name: string, tor: Buffer}");
+    else {
+      // console.log(`writing file: ${obj.name}.torrent`)
+      fs.writeFile(`${obj.name}.torrent`, obj.tor, err => {
+        if (err) reject(err);
+        else resolve(obj);
+      });
+    }
+  });
 }
 
 // Take an obj.name, and adds the torrent to transmission, assuming the torrent has been saved to the ./uploads directory
 function addFileToTransmission(obj) {
-    return new Promise((resolve, reject) => {
-        if (!(obj.b64 && obj.name)) reject(`addFileToTransmissionError: { b64:string, name: string }`)
-        else {
-            let torrentPath = `${obj.name}.torrent`;
-            // console.log(`torrent path: ${torrentPath}`);
-            transmission.addFile(torrentPath, { 'download-dir': `${obj.storagePath}` }, (err, arg) => {
-                if (err) reject(err);
-                else resolve(obj);
-            })
+  return new Promise((resolve, reject) => {
+    if (!(obj.b64 && obj.name))
+      reject(`addFileToTransmissionError: { b64:string, name: string }`);
+    else {
+      let torrentPath = `${obj.name}.torrent`;
+      // console.log(`torrent path: ${torrentPath}`);
+      transmission.addFile(
+        torrentPath,
+        { "download-dir": `${obj.storagePath}` },
+        (err, arg) => {
+          if (err) reject(err);
+          else resolve(obj);
         }
-    })
+      );
+    }
+  });
 }
 
-
-// testing the flow:
-// createTorrentFromFileName('2019-07-10-raspbian-buster-full.zip')
-//     .then(o => saveFileToTorrentFolder(o))
-//     .then(o => addFileToTransmission(o))
-//     .catch(e => console.log(e))
-
-module.exports = { 
-    createTorrentFromFileName, 
-    saveFileToTorrentFolder, 
-    addFileToTransmission 
+module.exports = {
+  createTorrentFromFileName,
+  saveFileToTorrentFolder,
+  addFileToTransmission
 };
-
-
-
 
 // ----------------------------------------------
 
 // const MongoClient = require('mongodb').MongoClient;
 // const assert = require('assert');
 // const pipeline = [{}]
- 
+
 // // Connection URL
 // const url = 'mongodb://localhost:27017/pirateHub?replicaSet=rs0"';
- 
+
 // // Database Name
 // const dbName = 'pirateHub';
- 
+
 // // Use connect method to connect to the server
 // MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true }, (err, client) => {
 //     assert.equal(null, err);
@@ -100,9 +94,3 @@ module.exports = {
 //     //     })
 //     client.close();
 // });
-
-
-
-
-
-
