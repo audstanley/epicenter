9bc580519250fe9aabb3a61c9e08449accce0783
Erik Lipsky <eriklipsky@gmail.com>
2019-12-10 21:01:11 -0800

    Iteration 5

--- client/fileSearch.js
+++ client/fileSearch.js
@@ -4,49 +4,37 @@ import { ReactiveVar } from "meteor/reactive-var";
 import { Files } from "../imports/api/Files";
 import { createDeflate } from "zlib";
 import { parse } from "url";
-import { Session } from 'meteor/session'
-import { formatBytes } from "../imports/api/eriksFunctions"
+// import { searchBar } from "../imports/api/eriksFunctions"
 
 Template.fileSearch.helpers({
   allFiles() {
-    // var = global and let = local
-
-    var allFilesFromMongo = Files.find().fetch();
-    var allFilesFromMongoReversed = allFilesFromMongo.reverse();
-    var allFilesWithSizeString = 
-    allFilesFromMongoReversed.map(fileObj => {
-      return Object.assign(fileObj, { 
+    let allFilesFromMongo = Files.find().fetch();
+    let allFilesFromMongoReversed = allFilesFromMongo.reverse();
+    let allFilesWithSizeString = allFilesFromMongoReversed.map(fileObj => {
+      return Object.assign(fileObj, {
         fDate: fileObj.meta.created_at,
-        //fUser: Meteor.user().emails.address[0],
-        fUser: findUsers(fileObj.userId),
+        fUser: Meteor.user().emails[0].address,
         fSize: formatBytes(fileObj.size),
-        fName: fileObj.name,
+        //fUser: findUser(fileobj.userId)
       });
     });
-    console.log(allFilesWithSizeString);
     return allFilesWithSizeString;
+    
+    // return original uploader
+    // 
+    // db.Files.find().pretty()
+    // "userId" : "AMtKtJKCrhYp9C5iH",
+    // db.users.find().pretty()
+    // "_id" : "AMtKtJKCrhYp9C5iH",
+    //
+    // function findUser(tempuser){
+    //   if (this.userId == ) {
+    //     return Meteor.users.find({ _id: this.userId };      
+    // };
 
-    /*
-    db.Files.find().pretty()
-    "_id" : "QXcrq6dnQzJLN9gMk",
-    "userId" : "AMtKtJKCrhYp9C5iH",
-
-    db.users.find().pretty()
-    "_id" : "FNbYxMNvGZMktffhK",
-    "_id" : "AMtKtJKCrhYp9C5iH",
-    */
-    function findUsers(tUser){
-      //let d = Meteor.user().emails[0].address;
-      //let uID = Meteor.users.userId;
-      //Meteor.user().emails[0].address;
-      return Meteor.users.findOne(userId)
-      return Meteor.users.findOne(this.tUser)
-
-      // if user ID's match
-      if (fID == uID)
-        return fUser;
-      else
-        return "N/A";
+    // doesn't work outside allFiles()
+    function formatBytes(bytes) {
+      return (bytes / 1048576).toFixed(2) + " MB";
     };
   },
 });
