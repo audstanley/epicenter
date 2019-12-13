cf7751be26c675c0157294210086223da2219970
Richard Stanley <audstanley@gmail.com>
2019-12-12 16:14:00 -0800

    .

--- client/fileUpload.js
+++ client/fileUpload.js
@@ -78,6 +78,70 @@ Template.fileUpload.events({
       upload.start();
     }
   },
+  "change #fileInDrop"(event, templateInstance) {
+    if (event.currentTarget.files && event.currentTarget.files[0]) {
+      // We upload only one file, in case
+      // multiple files were selected
+      const upload = Files.insert(
+        {
+          file: event.currentTarget.files[0],
+          streams: "dynamic",
+          chunkSize: "dynamic"
+        },
+        false
+      );
+
+      upload.on("start", function() {
+        templateInstance.currentUpload.set(this);
+      });
+
+      upload.on("end", function(error, fileObj) {
+        if (error) {
+          alert("Error during upload: " + error);
+          $(`#uploadFeedback`).html(
+            `<div class="unsuccessfulUpload">
+            <p>Error uploading: <strong>${fileObj.name}</strong></p>
+            </div>`
+          );
+          $(`.unsuccessfulUpload`).css({ "background-color": "#FFD9CD" });
+        } else {
+          //alert('File "' + fileObj.name + '" successfully uploaded');
+
+          $(`#uploadFeedback`).html(
+            `<div class="successfulUpload" id="successfulUpload">
+              <p>Successfully uploaded:
+              <strong>${fileObj.name}</strong></p>
+            </div>`
+          );
+
+          Velocity(
+            document.getElementsByClassName("successfulUpload"),
+            { opacity: 0 },
+            { duration: 0 }
+          );
+          Velocity(
+            document.getElementsByClassName("successfulUpload"),
+            { opacity: 0.8 },
+            { duration: 1000, delay: 200 }
+          );
+
+          //$(`.successfulUpload`).css({ "background-color": "#DEFFEC" });
+
+          Meteor.call(
+            "makeTorrentFromUploadedFile",
+            fileObj,
+            (error, result) => {
+              if (error) console.log(error);
+              else console.log(`makeTorrentFromUploadFileCall: ${result}`);
+            }
+          );
+        }
+        templateInstance.currentUpload.set(false);
+      });
+
+      upload.start();
+    }
+  },
   "mouseenter #uploadBoxArea2": (event, templateInstance) => {
     console.log("mousehover", event);
   }
--- imports/api/messages.js
+++ imports/api/messages.js
@@ -1,2 +1,6 @@
-import { Mongo } from 'meteor/mongo';
-export const Messages = new Mongo.Collection('messages');
+import { Mongo } from "meteor/mongo";
+
+//** This function returns 5 */
+let a = () => 5;
+
+export const Messages = new Mongo.Collection("messages");
