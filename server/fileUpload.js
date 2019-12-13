dc410ddc93df2a059c335de0a6bd03dccd21c271
Richard Stanley <audstanley@gmail.com>
2019-12-12 16:14:17 -0800

    .

--- server/fileUpload.js
+++ server/fileUpload.js
@@ -6,14 +6,36 @@ import {
   createTorrentFromFileName
 } from "./torrentFunctions/torrentFunctions";
 import { launchTransmission } from "../imports/api/launchTransmission";
+import { launchDocumentationJs } from "../imports/api/launchDocumenationJs";
+import { hostname } from "os";
+const computerHostname = hostname();
 
-launchTransmission();
+try {
+  launchTransmission();
+} catch (e) {
+  console.log(`EpicenterError: ${e}\n\n`);
+  console.log(
+    `Could not launch Transmission, You likely need to install: https://transmissionbt.com/download/ before running meteor`
+  );
+}
+
+try {
+  launchDocumentationJs();
+} catch (e) {
+  console.log(`EpicenterError: ${e}\n\n`);
+  console.log(
+    `Could not launch documentation.js, You likely need to install on Linux: sudo npm i -g documentation.js`
+  );
+  console.log(`Or on Windows: npm i -g documentation.js`);
+}
+
 const Files = new FilesCollection({
   collectionName: "Files",
   allowClientCode: false, // Disallow remove files from Client
   debug: true,
   storagePath: "../../../../../public/Files",
   onBeforeUpload(file) {
+    file.hostname = computerHostname;
     return true;
   }
 });
